491a614c10b4bcec33d9504f0dec7249ab32fe4c
Codrut Grosu <codrut.cristian.grosu@gmail.com>
2021-12-19 22:28:49 +0200

    main: print the version from the cmake file.

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+#include "Version.h"
+
 int main () {
 
+	std::cout << "Version " << StockPortofolio_VERSION_MAJOR << ".";
+	std::cout << StockPortofolio_VERSION_MINOR << "\n";
 	std::cout << "Hello world!\n";
 	return 0;
 }
