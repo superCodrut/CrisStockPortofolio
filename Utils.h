4ddc6ea62a3cf5f501ff7d7e0b331d576d50674b
Codrut Grosu <codrut.cristian.grosu@gmail.com>
2022-02-12 15:05:55 +0200

    utils: add TransactionInformation enum and update the other enums.

--- Utils.h
+++ Utils.h
@@ -1,7 +1,9 @@
 #ifndef _UTILS_H__
 #define _UTILS_H__
 
-enum TransactionType {BUY, SELL};
-enum Curency {USD, RON, EURO};
+enum TransactionType {BUY, SELL, NON_TYPE};
+enum Curency {USD, RON, EURO, NON_CURRENCY};
+enum TransactionInformation {COMPANY, SYMBOL, TYPE, CURENCY,
+	CURENCY_CONVERSION, DAY, MONTH, YEAR, NO_SHARES, PRICE, COMISSION};
 
 #endif
